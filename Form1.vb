fc226e4bd7721a62228394528003822c8ea115d2
Barbara Ormonde <bmo25691@gmail.com>
2018-09-18 17:05:58 +0000

    Initial commit

---
+++ Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+
+    Dim currentTime As String
+    Dim messageTime As String
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        currentTime = TimeOfDay.ToString("hh:mm:ss tt")
+        Label1.Text = currentTime
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        messageTime = MaskedTextBox1.Text + " " + ComboBox1.Text
+        Label4.Text = "Reminder set for: " + messageTime
+
+        If currentTime = messageTime Then
+            Timer2.Stop()
+            MsgBox(TextBox1.Text)
+            Button1.Enabled = True
+            Button2.Enabled = False
+            Label4.Text = ""
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer2.Start()
+        Button1.Enabled = False
+        Button2.Enabled = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer2.Stop()
+        Button1.Enabled = True
+        Button2.Enabled = False
+        Label4.Text = ""
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        NotifyIcon1.Visible = True
+        Me.Hide()
+        NotifyIcon1.ShowBalloonTip(3000)
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        Me.Show()
+        NotifyIcon1.Visible = False
+    End Sub
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Simple Reminder")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Simple Reminder")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e384b015-ff0a-4cca-bde5-631b25b2e46c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
